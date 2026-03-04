5be96b62081f3f2e319356b1c820e65b1eb106b2
Shiva_thinik_debian <alcatelandry8@gmail.com>
2026-03-05 02:38:37 +0300

    bin tree init and insert

--- sine_scaler/main.c
+++ sine_scaler/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 int sine_table[] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6, 7, 7, 8, 9, 9, 10, 11, 12, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 35, 36, 37, 38, 39, 40, 42, 43, 44, 45, 46, 48, 49, 50, 51, 52, 54, 55, 56, 57, 58, 60, 61, 62, 63, 64, 65, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 86, 87, 88, 89, 90, 90, 91, 92, 92, 93, 93, 94, 95, 95, 96, 96, 96, 97, 97, 98, 98, 98, 98, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 98, 98, 98, 98, 97, 97, 96, 96, 96, 95, 95, 94, 94, 93, 92, 92, 91, 90, 90, 89, 88, 87, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 64, 63, 62, 61, 60, 59, 57, 56, 55, 54, 53, 51, 50, 49, 48, 47, 45, 44, 43, 42, 41, 39, 38, 37, 36, 35, 34, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 13, 12, 11, 10, 9, 9, 8, 7, 7, 6, 6, 5, 4, 4, 3, 3, 3, 2, 2, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0};
 int size = sizeof(sine_table)/sizeof(sine_table[0]);
-#define duty 0
+  
+#define duty 100
 
 
 void comp(int _duty,int size_of_table ,int table[]);
@@ -11,6 +13,8 @@ void plot(int sine_table[],int num_of_div);
 void plot_3ph(int sine_table1[],int sine_table2[],int sine_table3[],int num_of_div);
 int main(){
     comp(duty, size, sine_table);
+    int test = 9/2;
+    printf("%d",test);
     return 0;
 }
 typedef struct table_characteristic
@@ -25,7 +29,28 @@ typedef struct table_characteristic
 } table_characteristic;
 
 
+void binary_comp(int _duty,int size_of_table ,int table[]){
+  table_characteristic input_table;
+  
+  input_table.first_el = 0;
+  input_table.end_el = size_of_table - 1;
+  input_table.right_middle = _duty/2;
+  input_table.left_middle = input_table.right_middle - 1;
+  input_table.size_of_compressed_table= size_of_table;
 
+}
+
+int binary_map(int _duty, int size_of_table, int table[]){
+  table_characteristic input_table;
+  input_table.first_el = 0;
+  input_table.end_el = size_of_table - 1;
+  input_table.right_middle = _duty/2;
+  input_table.left_middle = input_table.right_middle - 1;
+  // input_table.size_of_compressed_table = ;
+
+}
+
+ 
 void comp(int _duty,int size_of_table ,int table[]){
     table_characteristic input_table;
     input_table.first_el = 0;
@@ -70,12 +95,8 @@ void comp(int _duty,int size_of_table ,int table[]){
         }
         
     }
-    
-    
     plot(first_compressed_table,input_table.size_of_compressed_table);
-    plot(second_compressed_table,input_table.size_of_compressed_table);
-    plot(third_compressed_table,input_table.size_of_compressed_table);
-    plot_3ph(first_compressed_table,second_compressed_table,third_compressed_table,input_table.size_of_compressed_table);
+    
 }
 
 void plot(int sine_table[],int num_of_div){
@@ -138,4 +159,61 @@ void plot_3ph(int sine_table1[],int sine_table2[],int sine_table3[],int num_of_d
         }
         printf("\n");
     }
+}
+
+typedef struct node{
+  int pos, count, childs_counts_left, childs_counts_right;
+  struct node *mother;
+  struct node *left;
+  struct node *right
+}Node;
+
+typedef struct bin_tree{
+  int seed;
+  Node *seed_node;
+}BinTree;
+
+Node* CreateNode(int position,Node *mother){
+  /*
+  position - позиция в исходной таблице, основное значение
+  
+  */ 
+  if(position < 0 ) return NULL;
+  Node* Nu =(BinTree*)malloc(sizeof(BinTree));
+  if(Nu == NULL) return NULL;
+  Nu->pos = position;
+  Nu->childs_counts_left = 0;
+  Nu->childs_counts_right = 0;
+  Nu->left = NULL;
+  Nu->right = NULL;
+  Nu->mother = mother;
+} 
+
+int insert(BinTree *Bt, int position, int count){
+  Node* transfer;
+  if(Bt -> seed_node == NULL){
+    Bt->seed = position;
+    Bt->seed_node =CreateNode(position,NULL);
+    Bt->seed_node -> childs_counts_right = CreateNode(NULL,NULL);
+  }
+  else{
+    transfer = Bt->seed_node;
+    if(transfer->mother == NULL ){
+      transfer = transfer->childs_counts_left;
+    }
+    while(1){
+      if(transfer->childs_counts_left == transfer->childs_counts_right && transfer->left != NULL && transfer->right !=NULL){
+        transfer = transfer->left; 
+      }
+      else if(transfer->childs_counts_left > transfer->childs_counts_right && transfer->left != NULL && transfer->right !=NULL){
+        transfer = transfer->right;
+      }
+      if(transfer->childs_counts_left == 0 ||  transfer->childs_counts_right == 0){
+        break;
+      }
+    }
+    if(transfer->childs_counts_left == 0 && transfer->childs_counts_right == 0){
+      transfer->left = CreateNode(position,transfer);
+    }
+  }
 }
